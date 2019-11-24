a3df199286027235bc4750d80ea3e0616b1c702c
Danilo Martinelli <danilomartinelli429@gmail.com>
2019-11-24 19:14:59 -0300

    feat: add search term and prefix

---
+++ src/modules/userInput.ts
@@ -0,0 +1,17 @@
+import readline from "readline-sync";
+
+export function askAndReturnSearchTerm() {
+  return readline.question("Type a Wikipedia search term: ");
+}
+
+export function askAndReturnPrefix() {
+  const prefixes = ["Who is", "What is", "The history of"];
+
+  const selectedPrefixIndex = readline.keyInSelect(
+    prefixes,
+    "Choose one option: ",
+    { cancel: false }
+  );
+
+  return prefixes[selectedPrefixIndex];
+}
