fe402e33bda2e68b73abd536a8ec5e13e4d5877b
Danilo Martinelli <danilomartinelli429@gmail.com>
2019-12-04 09:51:19 -0300

    fix: add content folder to gitignore

--- src/modules/state.ts
+++ src/modules/state.ts
@@ -2,11 +2,15 @@ import fs from "fs";
 import path from "path";
 
 const ROOT_DIR = path.resolve(__dirname);
+const contentFolder = path.resolve(ROOT_DIR, "../content");
 const contentFilePath = path.resolve(ROOT_DIR, "../content/content.json");
 
 export function save(data: any) {
   const contentString = JSON.stringify(data);
 
+  if (!fs.existsSync(contentFolder)) {
+    fs.mkdirSync(contentFolder);
+  }
   return fs.writeFileSync(contentFilePath, contentString);
 }
 
